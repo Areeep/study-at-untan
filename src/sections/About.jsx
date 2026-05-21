6720560e7f33b931ef23e2f58a0e1d494de87883
Syariffullah <dinnereatc@gmail.com>
2026-05-21 22:37:32 +0700

    fix(about): update square size and improve layout for better responsiveness

--- src/sections/About.jsx
+++ src/sections/About.jsx
@@ -1,28 +1,28 @@
 import Eyebrow from "../components/Eyebrow";
 
 const squareSize =
-  "h-37.5 w-37.5 sm:h-60 sm:w-60 md:h-70 md:w-70 lg:h-100 lg:w-100 xl:w-150 xl:h-150";
+  "h-[300px] w-[300px] sm:h-60 sm:w-60 md:h-70 md:w-70 lg:h-100 lg:w-100 xl:w-150 xl:h-150";
 
 export default function About() {
   return (
-    <section className="font-inter flex gap-6 px-3 py-5 sm:gap-8 sm:px-8 sm:py-8 md:px-8 md:py-12 lg:gap-16 lg:px-16 lg:py-24">
+    <section className="font-inter flex flex-col gap-6 px-3 py-5 sm:flex-row sm:gap-8 sm:px-8 sm:py-8 md:px-8 md:py-12 lg:gap-16 lg:px-16 lg:py-24">
       <div className={`relative ${squareSize}`}>
         <span
           className={`bg-cream absolute -right-3 -bottom-3 rounded-md lg:-right-6 lg:-bottom-6 lg:rounded-xl ${squareSize}`}
         ></span>
         <div
-          className={`${squareSize} relative z-10 bg-[url('/images/hero.jpeg')] bg-cover bg-center`}
+          className={`${squareSize} relative z-10 bg-[url('/images/untan.jpeg')] bg-cover bg-center`}
         ></div>
       </div>
 
       <div className="flex flex-col gap-2 lg:gap-6 xl:gap-8">
         <Eyebrow text="Tentang Untan" />
 
-        <h2 className="text-navy text-sm font-black tracking-tighter sm:text-xl md:text-xl lg:text-4xl xl:text-5xl">
+        <h2 className="text-navy text-xl font-black tracking-tighter sm:text-xl md:text-xl lg:text-4xl xl:text-5xl">
           Kampus Unggul di Kalimantan Barat
         </h2>
 
-        <div className="text-gray space-y-2 text-[7px] font-medium sm:text-xs sm:leading-4 md:leading-6 lg:text-sm lg:leading-6 xl:text-lg xl:leading-8">
+        <div className="text-gray space-y-2 text-xs font-medium sm:text-xs sm:leading-4 md:leading-6 lg:text-sm lg:leading-6 xl:text-lg xl:leading-8">
           <p>
             Universitas Tanjungpura merupakan perguruan tinggi negeri terkemuka
             di Kalimantan Barat yang berkomitmen menghadirkan pendidikan
@@ -42,12 +42,12 @@ export default function About() {
           href="https://www.untan.ac.id/"
           target="_blank"
           rel="noopener noreferrer"
-          className="border-accent-dark text-navy flex w-fit items-center gap-0.5 border-b-2 text-[7px] font-bold sm:gap-1 sm:text-xs lg:text-sm"
+          className="border-accent-dark text-navy flex w-fit items-center gap-0.5 border-b-2 text-xs font-bold sm:gap-1 lg:text-sm"
         >
           Kunjungi Website Resmi{" "}
           <svg
             xmlns="http://www.w3.org/2000/svg"
-            className="mb-0.5 h-2.5 w-2.5 rotate-90 sm:h-3 sm:w-3 lg:h-5 lg:w-5"
+            className="mb-0.5 h-3 w-3 rotate-90 lg:h-5 lg:w-5"
             viewBox="0 0 24 24"
           >
             <path
